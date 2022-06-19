7d3b043ef118d23377ddafa179ceff43aa4ffbdd
Henry Nunez <henry.nunez@clabs.co>
2022-06-19 15:38:15 -0400

    created a addToHead method to cread a new head

--- LinkedList.js
+++ LinkedList.js
@@ -2,8 +2,16 @@ const Node = require('./Node');
 
 class LinkedList {
     constructor(){
-        
+        this.head = null
+    }
+    addToHead(data){
+        const newHead = new Node(data)
+        const currentHead = this.head
+        this.head = newHead
+        if(currentHead){
+            this.head.setNextNode(currentHead)
+        }
     }
 }
 
-module.export = LinkedList
+module.export = LinkedList;
