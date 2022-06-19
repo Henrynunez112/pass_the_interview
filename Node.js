e99729248ae631e6540b545dae8fc44964aabc05
Henry Nunez <henry.nunez@clabs.co>
2022-06-19 15:07:46 -0400

    adding some notes

--- Node.js
+++ Node.js
@@ -1,3 +1,14 @@
+/**
+ * Nodes Review
+ * 
+ *  -Contain data, which can be a variety of data types.
+ * 
+ *  -Contains links to other Nodes. If a node has no links, or they are all null
+ *   you have reached the end of the path you were following.
+ * 
+ *  -Can be orphaned if there are no existing links to them.
+ */
+
 class Node {
     constructor(data){
         this.data = data
