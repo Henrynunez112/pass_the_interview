6fcc4f85f7c1297f66c5fe8e34acac4ce0f750a9
Henry Nunez <henry.nunez@clabs.co>
2022-06-19 16:08:55 -0400

    created a addToTail function and some nodes

--- LinkedList.js
+++ LinkedList.js
@@ -1,4 +1,22 @@
 const Node = require('./Node');
+/**
+ * Linked List
+ * -Are compromised of Nodes
+ * 
+ * -The nodes contain a link to the next node
+ * (and also the previous node for bidirectional linked lists)
+ * 
+ * -Can be unidirectional or bidirectional
+ * 
+ * - Are a basic data structure and form the basis for many
+ * other data structure.
+ * 
+ * -Have a single head node, which serves as the first node
+ * in the list
+ * 
+ * -Require some maintenance in order to add or remove nodes
+ * 
+ */
 
 class LinkedList {
     constructor(){
@@ -10,6 +28,17 @@ class LinkedList {
         this.head = newHead
         if(currentHead){
             this.head.setNextNode(currentHead)
+        }
+    }
+    addToTail(data){
+        let tail = this.head
+        if(!tail){
+            this.head = new Node(data)
+        }else{
+            while(tail.getNextNode() !== null){
+                tail = tail.getNextNode()
+            }
+            tail.setNextNode(new Node(data))
         }
     }
 }
