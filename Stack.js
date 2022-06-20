fe6440b7b7b28a1833db63b16c316593bb763988
Henry Nunez <henry.nunez@clabs.co>
2022-06-20 16:56:48 -0400

    done creating Stack LinkedList

--- Stack.js
+++ Stack.js
@@ -6,15 +6,35 @@ class Stack {
         this.maxSize = maxSize
         this.size = 0
     }
+    hasRoom(){
+        return this.size < this.maxSize
+    }
+    isEmpty(){
+        return this.size === 0
+    }
     peak(){
-        if(this.size > 0) return this.stack.head
+        if(!this.isEmpty()){
+            if(this.size > 0) return this.stack.head
+        }else{
+            return null
+        }
     }
     push(value){
-        this.stack.addToHead(value)
+        if(this.hasRoom()){
+            this.stack.addToHead(value)
+            this.size++
+        }else{
+            throw new Error('Stack is full')
+        }
     }
     pop(){
-        const value = this.stack.removeHead()
-        return value
+        if(!this.isEmpty()){
+            const value = this.stack.removeHead()
+            this.size++
+            return value
+        }else{
+            throw new Error('Stack is empty')
+        }
     }
 }
 
