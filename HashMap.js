b5004dd0891f549a1763c32d59ccc562778bdf6f
Henry Nunez <henry.nunez@clabs.co>
2022-06-23 21:24:11 -0400

    created HashMap function

--- HashMap.js
+++ HashMap.js
@@ -24,28 +24,44 @@ Recipe for retrieving from a hash table:
 
 class HashMap {
   constructor(size = 0) {
-    this.hashmap = new Array(size).fill(null);
+    this.hashmap = new Array(size).fill(null).map(() => new LinkedList());
   }
   /**
    * The hashing function is the secret to efficiently storing and
    * retrieving values in a hash map. A hashing function takes a key
    * as input and returns an index within the hash map’s underlying array.
    */
-  hash(key){
-    let hashCode = 0
-    for(let i = 0; i < key.length; i++){
-        hashCode += hashCode + key.charCodeAt(i)
+  hash(key) {
+    let hashCode = 0;
+    for (let i = 0; i < key.length; i++) {
+      hashCode += hashCode + key.charCodeAt(i);
     }
-    return hashCode % this.hashmap.length
+    return hashCode % this.hashmap.length;
   }
 
   /**
    * .assign() will handle the logic needed to take in a key-value
    * pair and store the value at a particular index.
    */
-  assign(key, value){
-    const arrayIndex = this.hash(key)
-    this.hashmap[arrayIndex] = value
+  assign(key, value) {
+    const arrayIndex = this.hash(key);
+    const linkedList = this.hashmap[arrayIndex]
+    if(linkedList === null){
+        linkedList.addToHead({key, value});
+        return
+    }
+    let current = linkedList.head
+    while(current){
+      if(current.data.key === key){
+        current.data = {key, value}
+      }
+      if(!current.getNextNode()){
+        const newNode = new Node({key, value})
+        current.setNextNode(newNode)
+        break
+      }
+      current = current.getNextNode()
+    }
   }
 
   /**
@@ -55,9 +71,15 @@ class HashMap {
    * This method will make use of .hash()‘s deterministic nature to
    * find the value we’re looking for in the hash map.
    */
-  retrieve(key){
-    const arrayIndex = this.hash(key)
-    return this.hashmap[arrayIndex]
-    
+  retrieve(key) {
+    const arrayIndex = this.hash(key);
+    let current = this.hashmap[arrayIndex].head
+    while(current){
+      if(current.data.key === key){
+        return current.data.value
+      }
+      current = current.getNextNode()
+    }
+    return null
   }
 }
