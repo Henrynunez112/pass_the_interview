e6c9dde245e3009a0363fbbeeffcde937e09b9aa
Henry Nunez <henry.nunez@clabs.co>
2022-06-25 18:44:53 -0400

    added a demo function

--- TreeNode.js
+++ TreeNode.js
@@ -23,4 +23,43 @@ class TreeNode {
             this.children.forEach(child => child.removeChild(childToRemove))
         }
     }
+    
+    print(level = 0){
+        let result = ''
+        for(let i = 0; i < level; i++){
+            result += '-- '
+        }
+        console.log(`${result}${this.data}`)
+        this.children.forEach(child => child.print(level + 1))
+    }
 }
+
+// const tree = new TreeNode(1);
+// const randomize = () => Math.floor(Math.random() * 20);
+
+// // add first-level children
+// for (let i = 0; i < 3; i++) {
+//     tree.addChild(randomize());
+//   }
+  
+//   // add second-level children
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 2; j++) {
+//       tree.children[i].addChild(randomize());
+//     }
+//   }
+  
+//   // add third-level children
+//   for (let i = 0; i < 3; i++) {
+//     for (let j = 0; j < 2; j++) {
+//       for (let k = 0; k < 2; k++) {
+//         tree.children[i].children[j].addChild(randomize());
+//       }
+//     }
+//   }
+  
+//   // pretty-print the tree
+//   tree.print();
+  
+
+module.export = TreeNode
