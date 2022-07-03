ff7c16f9f8e2a1bf0173fe0c41f409bc03075255
Henry Nunez <henry.nunez@clabs.co>
2022-07-02 22:25:35 -0400

    added a bubbleSort

---
+++ BubbleSort.js
@@ -0,0 +1,21 @@
+const sorting = (arr, indexOne, indexTwo) =>{
+    const temp = arr[indexTwo]
+    arr[indexTwo] = arr[indexOne]
+    arr[indexOne] = temp
+}
+
+const bubbleSort = (arr) =>{
+    let swapping = true
+    while(swapping){
+        swapping = false
+        for(let i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                sorting(arr, i, i+1)
+                swapping = true
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([5, 6, 2, 3, 1, 4]))
